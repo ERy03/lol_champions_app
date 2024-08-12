a9c24404c17df1d422bd92385c98ea056d611442
Ryota <ryoenoki2013@gmail.com>
2024-08-12 16:44:05 +0900

    add: champions screen

---
+++ lib/screens/champions_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChampionsScreen extends StatelessWidget {
+  const ChampionsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: Text('Champions Screen')),
+    );
+  }
+}
